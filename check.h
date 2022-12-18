c76b6648e6372ef819b24b3216b948fed8bfd0fc
hanan saeed <hanansaeedbs12@gmail.com>
2022-12-18 23:30:12 +0200

    add check.h

---
+++ check.h
@@ -0,0 +1,12 @@
+#ifndef CHECK_H
+#define CHECK_H
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+string check_error(string xml);
+#endif // CHECK_H
